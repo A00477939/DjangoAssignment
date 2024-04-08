58fecb3d4f0888b824bbdab58551c83f71e50013
Hritik Shashi Arora <144352807+A00477939@users.noreply.github.com>
2024-04-07 23:10:44 -0300

    Admin View Added

--- DjangoHotel/urls.py
+++ DjangoHotel/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+
+from django.contrib import admin
 from .views import HotelListView
 
 urlpatterns = [
     path('hotels/', HotelListView.as_view(), name='hotel-list'),
+    path('admin/', admin.site.urls),
 ]
